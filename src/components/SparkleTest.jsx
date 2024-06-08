737c087495147740ae979b6fb42a8c0d16b4cf5c
fatbigcat <ks6489@student.uni-lj.si>
2024-06-08 10:23:16 +0200

    loading screen styling

---
+++ src/components/SparkleTest.jsx
@@ -0,0 +1,34 @@
+// SparkleTest.jsx
+import React from 'react';
+import Sparkles from './Sparkles';
+import { Html } from '@react-three/drei';
+
+const SparkleTest = () => {
+    return (
+        <Html>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh',
+                    background: '#83a4d4',
+                }}>
+                <Sparkles>
+                    <img
+                        src="src/assets/images/mai-col/town_colorAdjusted.png"
+                        alt="Top Slide"
+                        style={{
+                            background: 'none',
+                            border: 'none',
+                            width: '300px',
+                            height: '300px',
+                        }}
+                    />
+                </Sparkles>
+            </div>
+        </Html>
+    );
+};
+
+export default SparkleTest;
