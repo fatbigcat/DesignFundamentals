a46b8f8ee88ce00dc3f8b621ffccc1d87629d7d8
fatbigcat <ks6489@student.uni-lj.si>
2024-06-08 08:08:57 +0200

    minor cosmetic changes to customloader

--- src/components/CustomLoader.jsx
+++ src/components/CustomLoader.jsx
@@ -1,23 +1,36 @@
-// CustomLoader.jsx
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import '../index.css';
 
 const CustomLoader = ({ onClick }) => {
+    const [clickCount, setClickCount] = useState(0);
+
+    const handleClick = () => {
+        if (clickCount < 1) {
+            setClickCount(clickCount + 1);
+        } else {
+            if (onClick) onClick();
+        }
+    };
+
     return (
-        <div className="custom-loader" onClick={onClick}>
-            <div>
+        <div className="custom-loader" onClick={handleClick}>
+            {clickCount === 0 && (
                 <img
-                    src="src\assets\images\mai-col\town_colorAdjusted.png"
+                    src="src/assets/images/mai-col/town_colorAdjusted.png"
                     alt="Top Slide"
                     className="slide-top"
+                    style={{ background: 'none', border: 'none' }}
                 />
+            )}
+            {clickCount === 1 && (
                 <img
-                    src="src\assets\images\mai-col\parchment.png"
+                    src="src/assets/images/mai-col/parchment.png"
                     alt="Bottom Slide"
                     className="slide-bottom"
+                    style={{ background: 'none', border: 'none' }}
                 />
                 <p>Click anywhere to begin</p>
-            </div>
+            )}
         </div>
     );
 };
